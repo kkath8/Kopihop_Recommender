f97b67adfc6ecd91592e6b1154bffdb0781be89b
kkath8 <alvarezkath08@gmail.com>
2026-06-07 11:26:50 +0800

    update db.py

--- database/db.py
+++ database/db.py
@@ -7,7 +7,7 @@ def init_db(app):
     """Bind db to the Flask app and create all tables."""
     db.init_app(app)
     with app.app_context():
-        from database.models import Cafe, MenuItem, UserSession, VoiceLog, Favorite  # 👈 add this
+        from database.models import Cafe, MenuItem, UserSession, VoiceLog, Favorite 
         db.session.execute(db.text("CREATE EXTENSION IF NOT EXISTS vector;"))
         db.session.commit()
         db.create_all()
